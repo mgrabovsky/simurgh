bf42fed3caf74439307c2c03d4bcc510f99ff152
Matěj Grabovský <matej@mgrabovsky.net>
2019-08-20 16:27:37 +0200

    parser: Comment the individual subparsers

--- src/Simurgh/Parser.hs
+++ src/Simurgh/Parser.hs
@@ -30,10 +30,11 @@ ident      = P.identifier lexer
 parens     = P.parens lexer
 reserved   = P.reserved lexer
 reservedOp = P.reservedOp lexer
-whiteSpace = P.whiteSpace lexer
+whitespace = P.whiteSpace lexer
 
 arrow = reservedOp "=>"
 
+-- | Parser for an expression of the core lambda calculus.
 expr = do
     applicand <- atom
     args      <- many atom
@@ -41,6 +42,9 @@ expr = do
        then pure applicand
        else pure (App applicand args)
 
+-- | Parser for an atomic expression of the language, i.e. the @Set@ literal,
+-- a variable name, lambda abstraction, Pi type, the @let-in@ construct or
+-- a parenthesised expression.
 atom =  parens expr
     <|> reserved "Set" $> Set0
     <|> mkVar <$> ident
@@ -53,10 +57,18 @@ atom =  parens expr
               <*> (reserved "in" *> expr)
 
 -- TODO: Support syntax like `fun (x y : A) => _` for `fun (x : A) (y : A) => _`
+-- TODO: Parse `A -> B -> C` into `forall (_:A) (_:B) => C`,
+-- perhaps even Agda/Idris-like `(a:A) -> (b:B) -> C a b` for
+-- `forall (a:A) (b:B) => C A b`
+
+-- | Parser for a telescope of binders.
 binders = many1 binder
 
+-- | Parser for a single binder of the form @(x : A)@.
 binder = parens ((,) <$> ident <*> (colon *> expr))
 
+-- | Parse an expression of the core lambda calculus and return the result. The input
+-- file name is presumed to be "<stdin>".
 parseExpr :: String -> Either ParseError Expr
-parseExpr = parse (whiteSpace *> expr) "<stdin>"
+parseExpr = parse (whitespace *> expr) "<stdin>"
 
