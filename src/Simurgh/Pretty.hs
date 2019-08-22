67fe148bc9e9ca7f84a823f45caaf36160a0f534
Matěj Grabovský <matej@mgrabovsky.net>
2019-08-22 22:27:15 +0200

    pretty: Slightly handsomer pretty-printing

--- src/Simurgh/Pretty.hs
+++ src/Simurgh/Pretty.hs
@@ -6,7 +6,7 @@ import qualified Data.Set             as S
 import           Data.Set             (Set)
 import           Data.Typeable        (Typeable)
 import qualified Text.PrettyPrint     as PP
-import           Text.PrettyPrint     ((<+>), Doc)
+import           Text.PrettyPrint     ((<+>), Doc, colon, comma, parens)
 
 import           Unbound.Generics.LocallyNameless
 import           Unbound.Generics.LocallyNameless.Internal.Fold (toListOf)
@@ -20,6 +20,8 @@ import Simurgh.Syntax
 
 -- | Pretty printing for the new core syntax.
 
+mapsto = PP.text "=>"
+
 fvSet :: (Alpha a, Typeable b) => a -> Set (Name b)
 fvSet = S.fromList . toListOf fv
 
@@ -30,13 +32,13 @@ instance Pretty Expr where
   pprint (Var x)     = pure (PP.text (show x))
   pprint (App t1 ts) =
       let prettyTerms = traverse pprint (t1:ts)
-       in PP.parens <$> (foldr1 (<+>) <$> prettyTerms)
+       in parens <$> (foldr1 (<+>) <$> prettyTerms)
   pprint (Lam b)     = lunbind b $ \(tele, t) -> do
       prettyTele <- pprint tele
-      ((PP.text "λ" <+> prettyTele <+> PP.text "=>" <+>) <$> pprint t)
+      ((PP.text "λ" <+> prettyTele <+> mapsto <+>) <$> pprint t)
   pprint (Pi b)     = lunbind b $ \(tele, t) -> do
       prettyTele <- pprint tele
-      ((PP.text "Π" <+> prettyTele <+> PP.text "=>" <+>) <$> pprint t)
+      ((PP.text "Π" <+> (prettyTele <> comma) <+>) <$> pprint t)
   pprint (Let b)    = lunbind b $ \((x, Embed t), u) -> do
       let prefix = PP.text "let" <+> PP.text (show x) <+> PP.text "="
       ((\t u -> prefix <+> t <+> PP.text "in" <+> u) <$> pprint t <*> pprint u)
@@ -45,9 +47,9 @@ instance Pretty Expr where
 instance Pretty Telescope where
   pprint Empty = pure PP.empty
   pprint (Cons (unrebind -> ((x, Embed t), rest))) =
-      let ppVar  = PP.text (show x) <+> PP.colon 
+      let ppVar  = PP.text (show x) <+> colon
           ppItem = (ppVar <+>) <$> pprint t
-       in (<+>) <$> (PP.parens <$> ppItem) <*> pprint rest
+       in (<+>) <$> (parens <$> ppItem) <*> pprint rest
 
 prettyPrint :: Pretty a => a -> String
 prettyPrint = PP.render . runLFreshM . pprint
